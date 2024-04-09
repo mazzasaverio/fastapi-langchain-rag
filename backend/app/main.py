e4613a8f139c4ec3dfe9d4384dc4662a4b57b9ed
mazzasaverio <saverio3107@gmail.com>
2024-04-09 17:14:46 +0200

    UPDATE

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,9 +15,7 @@ app = FastAPI(
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[
-            str(origin).strip("/") for origin in settings.BACKEND_CORS_ORIGINS
-        ],
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
